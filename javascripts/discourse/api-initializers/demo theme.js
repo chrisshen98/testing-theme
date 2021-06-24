ead7acc4e293ae0d761f634fdd87711d2d950483
Keegan George <kgeorge13@gmail.com>
2021-06-24 10:51:34 -0700

    Move JS to initializer

--- javascripts/discourse/api-initializers/demo theme.js
+++ javascripts/discourse/api-initializers/demo theme.js
@@ -1,5 +1,15 @@
-import { apiInitializer } from "discourse/lib/api";
+import { apiInitializer } from 'discourse/lib/api';
 
-export default apiInitializer("0.11.1", api => {
-  console.log("hello world from api initializer!");
+export default apiInitializer('0.11.1', (api) => {
+  console.log('hello world from api initializer!');
+
+  api.onPageChange(() => {
+    document.getElementById('changeGreen').onclick = function () {
+      document.getElementById('output').style.color = 'green';
+    };
+
+    document.getElementById('changeRed').onclick = function () {
+      document.getElementById('output').style.color = 'red';
+    };
+  });
 });
