d545b2c0b98d9b8fe1f1e7d6dac43355abbecb17
Chris Shen <chrissh1124@gmail.com>
2021-06-24 16:00:06 -0500

    flip color button

--- javascripts/discourse/api-initializers/demo theme.js
+++ javascripts/discourse/api-initializers/demo theme.js
@@ -5,11 +5,13 @@ export default apiInitializer('0.11.1', (api) => {
 
   api.onPageChange(() => {
     document.getElementById('changeGreen').onclick = function () {
-      document.getElementById('output').style.color = 'green';
+      document.getElementById('output').style.color = '#88b98f';
+      document.getElementById('output').style.background = 'white';
     };
 
-    document.getElementById('changeRed').onclick = function () {
-      document.getElementById('output').style.color = 'red';
+    document.getElementById('changeWhite').onclick = function () {
+      document.getElementById('output').style.color = 'white';
+      document.getElementById('output').style.background = '#88b98f';
     };
   });
 });
