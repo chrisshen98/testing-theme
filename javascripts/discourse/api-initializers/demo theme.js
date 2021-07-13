a501b1cfaeed85a286b385a4f4c9101bf7dfd05f
Chris Shen <chrissh1124@gmail.com>
2021-07-13 13:45:22 -0500

    now supporting using toggle button to switch the theme but only color palette

--- javascripts/discourse/api-initializers/demo theme.js
+++ javascripts/discourse/api-initializers/demo theme.js
@@ -1,8 +1,7 @@
 import { apiInitializer } from 'discourse/lib/api';
 
 export default apiInitializer('0.11.1', (api) => {
-  console.log('hello world from api initializer!');
-  const { iconNode } = require("discourse-common/lib/icon-library");
+  console.log('hello world from api initializer!'); 
   
   api.onPageChange(() => {
     document.getElementById('themeCheckbox').onclick = function() {
@@ -10,26 +9,26 @@ export default apiInitializer('0.11.1', (api) => {
       if (cur.checked) {
         console.log('change to black');
         for (let i = 0; i < parameters.length; i++) {
-          document.documentElement.style.setProperty('--' + parameters[i], '#' + latte_theme[i]);
+          document.documentElement.style.setProperty('--' + parameters[i], '#' + dark_theme[i]);
         };
 
         elems = document.getElementsByClassName("themes")
         for (let i = 0; i < elems.length; i++) {
           elems[i].style.backgroundColor = "transparent";
-          if (elems[i].id == 'latte_theme') {
+          if (elems[i].id == 'dark_theme') {
             elems[i].style.backgroundColor = "var(--quaternary)";
           }
         }
       } else {
         console.log('change to red');
         for (let i = 0; i < parameters.length; i++) {
-          document.documentElement.style.setProperty('--' + parameters[i], '#' + summer_theme[i]);
+          document.documentElement.style.setProperty('--' + parameters[i], '#' + default_theme[i]);
         };
 
         elems = document.getElementsByClassName("themes")
         for (let i = 0; i < elems.length; i++) {
           elems[i].style.backgroundColor = "transparent";
-          if (elems[i].id == 'summer_theme') {
+          if (elems[i].id == 'default_theme') {
             elems[i].style.backgroundColor = "var(--quaternary)";
           }
         }
